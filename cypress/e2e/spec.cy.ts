66d944a8821900b870108653a1653c7c76fc8a92
Jérémy Chatard <jchatard@breek.fr>
2023-05-16 09:41:34 +0200

    Make it fail to show the line number bug.

--- cypress/e2e/spec.cy.ts
+++ cypress/e2e/spec.cy.ts
@@ -2,5 +2,33 @@
 describe('page', () => {
   it('works', () => {
     cy.visit('https://example.cypress.io')
+    cy.contains('Commands')
+  })
+
+  it('works', () => {
+    cy.visit('https://example.cypress.io')
+    cy.contains('Commands')
+  })
+
+
+  it('works', () => {
+    cy.visit('https://example.cypress.io')
+    cy.contains('Commands')
+  })
+
+
+  it('works', () => {
+    cy.visit('https://example.cypress.io')
+    cy.contains('Commands')
+  })
+
+  it('works', () => {
+    cy.visit('https://example.cypress.io')
+    cy.contains('Commands')
+  })
+
+  it('works', () => {
+    cy.visit('https://example.cypress.io')
+    cy.contains('CommandsNotFound')
   })
 })
